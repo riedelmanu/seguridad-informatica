d0839dc962836e28ae260146734d1f8fed6e10e0
Juani <juanignacioch4@gmail.com>
2026-03-27 22:35:15 -0300

    feat: manual cherry-pick of latest teacher repo

---
+++ app/store/students/types.ts
@@ -0,0 +1,12 @@
+export interface Student {
+  id: number
+  name: string
+  email: string
+  active: boolean
+}
+
+export interface StudentsStore {
+  students: Student[]
+
+  setStudents: (students: Student[]) => void
+}
